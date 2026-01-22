7a7cb9666b513bc1765c7a6a7b91bab7a28d4b3a
ROS Developer <user@example.com>
2026-01-22 08:49:27 +0800

    chore: remove tracked build artifacts and node_modules from repo

--- sql/les_db.sql
+++ sql/les_db.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80041 (8.0.41)
  Source Host           : localhost:3306
- Source Schema         : tansci_boot
+ Source Schema         : les_db
 
  Target Server Type    : MySQL
  Target Server Version : 80041 (8.0.41)
--- tansci-boot-ui-editor/gh-pages/config.js
+++ tansci-boot-ui-editor/gh-pages/config.js
@@ -1,1 +1,1 @@
-window.common={API_QUERY_URL:"http://localhost:7000/tansci/lcPages/get/",API_SAVE_URL:"http://localhost:7000/tansci/lcPages/update",TOKEN_KEY:"token"};
+window.common={API_QUERY_URL:"http://localhost:7000/tansci/lowcode/lcPages/get/",API_SAVE_URL:"http://localhost:7000/tansci/lowcode/lcPages/update",TOKEN_KEY:"token"};
